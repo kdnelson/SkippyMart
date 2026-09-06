e6d0ef7426ff1dbc446df2fae3131b4adb6a8482
Kris Nelson <kris.d.nelson@gmail.com>
2026-09-05 19:52:50 -0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,6 +15,6 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-west-2"
     encrypt = true
-    dynamodb_table = "skippymart-tf-lock"
+    use_lockfile = "skippymart-tf-lock"
   }
 }
